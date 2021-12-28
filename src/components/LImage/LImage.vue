7588e216cdcc1837c3e328d3defd95ef4c9e9b6c
1217967340@qq.com <1217967340@qq.com>
2021-12-28 23:09:13 +0800

    feat: 初始化组件库

---
+++ src/components/LImage/LImage.vue
@@ -0,0 +1,45 @@
+<template>
+  <component
+    :is="type"
+    :style="styleProps"
+    @click.prevent="handleClick"
+    class="l-image-component"
+    :src="src"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import {
+  transformToComponentProps,
+  imageDefaultProps,
+  imageStylePropsNames,
+} from "../../shared/defaultProps";
+import useCommonComponent from "../../hooks/useCommonComponent";
+
+const defaultProps = transformToComponentProps(imageDefaultProps);
+
+export default defineComponent({
+  name: "LImage",
+  props: {
+    ...defaultProps,
+  },
+  setup(props) {
+    const { styleProps, handleClick } = useCommonComponent(
+      props,
+      imageStylePropsNames
+    );
+    return {
+      styleProps,
+      handleClick,
+      type: "img",
+    };
+  },
+});
+</script>
+<style scoped>
+.l-image-component {
+  max-width: 100%;
+  position: relative !important;
+}
+</style>
---
+++ src/components/LText/LText.vue
@@ -0,0 +1,38 @@
+<template>
+  <component :is="tag" :style="styleProps" @click="handleClick">
+    {{ text }}
+  </component>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import {
+  transformToComponentProps,
+  TextComponentProps,
+  textDefaultProps,
+} from "../../shared/defaultProps";
+import useCommonComponent from "../../hooks/useCommonComponent";
+
+const defaultProps = transformToComponentProps(TextComponentProps);
+
+export default defineComponent({
+  name: "LText",
+  props: {
+    tag: {
+      type: String,
+      default: "span",
+    },
+    ...defaultProps,
+  },
+  setup(props) {
+    const { styleProps, handleClick } = useCommonComponent(
+      props,
+      textDefaultProps
+    );
+    return {
+      styleProps,
+      handleClick,
+    };
+  },
+});
+</script>
